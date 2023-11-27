e0682b4c9f6032ab5d6b65d619c7640024c3642f
tyler kim <yoonsoo.kim11@gmail.com>
2023-11-27 18:16:40 -0500

    done up to deliverable 3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
 // write your code here!
+let currentDuck
+fetch('http://127.0.0.1:3000/ducks')
+.then(resp => resp.json())
+.then(duckList => {
+    duckList.forEach(duck => {
+
+        const duckNavElement = document.getElementById('duck-nav')
+        
+        const imgElement = document.createElement('img')
+        imgElement.src = duck.img_url
+        duckNavElement.appendChild(imgElement)
+        
+
+        imgElement.addEventListener('click', e => {
+            const duckNameElement = document.getElementById('duck-display-name') 
+            const duckImageElement = document.getElementById('duck-display-image')
+            const duckButtonElement = document.getElementById('duck-display-likes')
+
+            duckNameElement.textContent = duck.name
+            duckImageElement.src = duck.img_url
+            duckButtonElement.textContent = duck.likes + ' likes'
+
+        })
+        
+    })
+
+
+    const duckButtonElement = document.getElementById('duck-display-likes')
+    duckButtonElement.addEventListener('click', e => {
+
+        let currentLikes = Number(duckButtonElement.textContent.split(' ')[0])
+        currentLikes += 1
+        duckButtonElement.textContent = currentLikes + ' likes'
+
+    })
+})
+
