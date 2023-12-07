41bcc87b4be91b49a24eaf5085a76a7d949b25ea
tyler kim <yoonsoo.kim11@gmail.com>
2023-12-06 23:57:58 -0500

    practice. redo form section

--- src/index.js
+++ src/index.js
@@ -1,53 +1,64 @@
-// write your code here!
-let currentDuck
-fetch('http://127.0.0.1:3000/ducks')
-.then(resp => resp.json())
-.then(duckList => {
-    duckList.forEach(duck => {
-            renderInNav(duck)
-       
+const duckNavELement = document.getElementById('duck-nav')
+const url = 'http://127.0.0.1:4000/ducks'
+const detailName = document.getElementById('duck-display-name')
+const detailImage = document.getElementById('duck-display-image')
+const detailLikes = document.getElementById('duck-display-likes')
+const newDuckForm = document.getElementById('new-duck-form')
+ let currentDuck
 
-        
+init(url)
+    .then(r => r.json())
+    .then(ducks => {
+        ducks.map(duck =>{
+            renderDuckImage(duck)
+        })
     })
 
+function renderDuckImage(duck){
+    console.log(duck)
+    const imgElement = document.createElement('img')
+    imgElement.src = duck.img_url
+    duckNavELement.append(imgElement)
 
-    const duckButtonElement = document.getElementById('duck-display-likes')
-    duckButtonElement.addEventListener('click', e => {
-        let currentLikes = Number(duckButtonElement.textContent.split(' ')[0])
-        currentLikes += 1
-        duckButtonElement.textContent = currentLikes + ' likes'
+    imgElement.addEventListener('click', () =>{
+        displayDetail(duck)
     })
 
-    const newDuckForm = document.getElementById('new-duck-form')
-    newDuckForm.addEventListener('submit', e => {
+}
+
+function init(url){
+   return fetch(url)
+}
+
+function displayDetail(duck){
+    currentDuck = duck
+    detailName.textContent = duck.name
+    detailImage.src = duck.img_url
+    detailLikes.textContent = duck.likes + ' likes'
+}
+
+function likeIncrementor(){
+    detailLikes.addEventListener('click', () =>{
+        currentDuck.likes += 1
+        detailLikes.textContent = Number(currentDuck.likes) + ` likes`
+    })
+}
+likeIncrementor()
+
+function handleNewDuck(){
+    newDuckForm.addEventListener('submit', (e) =>{
+        //capture value of input fields with e.target index.value
+        //call renderimage function add like button starts with 0(number) likes, string interpolate
         e.preventDefault()
-        const newDuckInfo = {
+        let newDuck = {
             name: e.target[0].value,
             img_url: e.target[1].value,
-            likes: 0
+            likes: Number(0) 
         }
-        renderInNav(newDuckInfo)
+        renderDuckImage(newDuck)
     })
-})
-
-
-function renderInNav(duck){
-    currentDuck = duck
-const duckNavElement = document.getElementById('duck-nav')
-
-const imgElement = document.createElement('img')
-imgElement.src = duck.img_url
-duckNavElement.appendChild(imgElement)
-
-
-imgElement.addEventListener('click', e => {
-    const duckNameElement = document.getElementById('duck-display-name') 
-    const duckImageElement = document.getElementById('duck-display-image')
-    const duckButtonElement = document.getElementById('duck-display-likes')
-
-    duckNameElement.textContent = duck.name
-    duckImageElement.src = duck.img_url
-    duckButtonElement.textContent = duck.likes + ' likes'
-
-})
 }
+
+handleNewDuck()
+
+//diff bttw putting function on e vs in body
